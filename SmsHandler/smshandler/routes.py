63650574561710a763ed9bd9899108bcf777da4c
bryan b <bryanbarajas96@gmail.com>
2020-01-15 19:15:27 -0800

    routes db.commit bug

--- SmsHandler/smshandler/routes.py
+++ SmsHandler/smshandler/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect, request
 from flask_restful import Resource
 # from marshmallow import Schema, fields, pprint
 from smshandler import app, db, config
-from smshandler.models import Phone, Messages, Statistics
+from smshandler.models import Phone
 from smshandler.twiliohandler import TwilioHandler
 
 '''
@@ -58,47 +58,69 @@ def incoming():
             device = Phone.query.filter_by(phone=from_).first()
             body = body.lower()
 
+            if device is not None:
+                device_id = device.id
+
             # put messages in users
             # create new user to database
             if device is None:
                 device = Phone(from_)
                 db.session.add(device)
+                response = "Reply with begin to start using with 10 free credits"
                 db.session.commit()
-                device = Phone.query.filter_by(phone=from_).first()
 
             # user exists or added to database
-            if "begin" in body:
+            elif "begin" in body:
                 if not device.freecredits:
                     response = "10 credits applied," \
                                "\nSms send under char limit are free," \
                                "\nGo to sms section of bryanbar website for more"
                     device.credits += 10
                     device.freecredits = True
+                    device.sent += 1
+                    device.received += nummedia
+                    db.session.commit()
+
                 else:
                     response = "Please navigate to sms section of bryanbar website to get more credits or for help" \
                                "\nContinuous spam will result in blacklist"
+                    device.sent += 1
+                    device.received += nummedia
+                    db.session.commit()
 
             elif not device.freecredits:
                 response = "Reply with begin to get 10 free credits or visit bryanbar for more help"
+                device.sent += 1
+                device.received += nummedia
+                db.session.commit()
 
             elif device.credits > 0:
                 if "test" in body:
                     response = "test body reply"
                     device.credits -= 1
+                    device.sent += 1
+                    device.received += nummedia
+                    db.session.commit()
 
                 elif "credits" in body:
                     response = "Credits under number: " + str(device.credits)
+                    device.sent += 1
+                    device.received += nummedia
+                    db.session.commit()
 
                 else:
                     response = "Unrecognized command, reply with 'help' or visit bryanbar website and visit sms"
+                    device.sent += 1
+                    device.received += nummedia
+                    db.session.commit()
 
             else:
                 response = "Please head over to bryanbar website to add more credits or for help"
+                device.sent += 1
+                device.received += nummedia
+                db.session.commit()
 
             handler.createmessage(response, device.phone)
-            device.sent += 1
-            device.received += nummedia
-            db.session.commit()
 
             return '', 202
 
